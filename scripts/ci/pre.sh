6ba2c2be31d34736815b40b9cddc5e2499ef98fc
Uday Verma <uday.karan@gmail.com>
2014-05-08 08:12:04 -0500

    Add libc++ install instructions for clang CI

--- scripts/ci/pre.sh
+++ scripts/ci/pre.sh
@@ -29,6 +29,17 @@ fi
 if [ "$CXX" == "clang++" ]; then 
 	sudo apt-get install --allow-unauthenticated -qq clang-3.4;
 	export CXX="clang++-3.4";
+
+	# Download and install libc++
+	export CXXFLAGS="-std=c++0x -stdlib=libc++" ;
+	svn co --quiet http://llvm.org/svn/llvm-project/libcxx/trunk libcxx ;
+
+	cd libcxx/lib && bash buildit;
+	sudo cp ./libc++.so.1.0 /usr/lib/;
+	sudo mkdir /usr/include/c++/v1;
+	cd .. && sudo cp -r include/* /usr/include/c++/v1/;
+	cd /usr/lib && sudo ln -sf libc++.so.1.0 libc++.so;
+	sudo ln -sf libc++.so.1.0 libc++.so.1 && cd $cwd;
 fi
 
 cd $TRAVIS_BUILD_DIR
