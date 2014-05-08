203cc42246bbfa6d77a8994d7cb4c1b2ecd9254f
Uday Verma <uday.karan@gmail.com>
2014-05-07 20:01:34 -0500

    Add compiler exports to run script

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -5,6 +5,16 @@
 mkdir -p _build || exit 1
 cd _build || exit 1
 
+# g++4.8.1
+if [ "$CXX" = "g++" ]; then 
+	export CXX="g++-4.8";
+fi
+
+# clang 3.4
+if [ "$CXX" == "clang++" ]; then 
+	export CXX="clang++-3.4";
+fi
+
 cmake \
 	-DCMAKE_BUILD_TYPE=Release \
 	..
