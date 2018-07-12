a269d582196bde51e9d3f5c69f00e56c5f503756
Howard Butler <howard@hobu.co>
2018-07-12 08:01:41 -0500

    increase version to 1.3.0

--- python/lazperf/__init__.py
+++ python/lazperf/__init__.py
@@ -1,4 +1,4 @@
-__version__='1.2.1'
+__version__='1.3.0'
 from .pylazperfapi import PyDecompressor as Decompressor
 from .pylazperfapi import PyCompressor as Compressor
 from .pylazperfapi import PyVLRDecompressor as VLRDecompressor
