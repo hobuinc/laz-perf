c9a5d28c909894edad0081e3d3b1cdaae89f7026
Howard Butler <howard@hobu.co>
2019-09-04 10:55:46 -0500

    __NUMPY_SETUP__ check

--- python/setup.py
+++ python/setup.py
@@ -32,7 +32,11 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        try:
+            __builtins__.__NUMPY_SETUP__ = False
+        except AttributeError:
+            __builtins__['__NUMPY_SETUP__'] = False
+
         import numpy
         self.include_dirs.append(numpy.get_include())
 ext = ".pyx" if USE_CYTHON else ".cpp"
