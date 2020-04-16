3337585a34261fef854ebb2181c0039abf48e4c5
Howard Butler <howard@hobu.co>
2020-04-16 11:58:05 -0500

    move sdist to packaging script

--- python-wheel-build.sh
+++ python-wheel-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /src/python
+/opt/python/cp37-cp37m/bin/python setup.py sdist
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 auditwheel -v repair ./dist/*linux_x86_64.whl
 rm ./dist/*linux_x86_64.whl
