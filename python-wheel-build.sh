e715a4bcb7c80d8c0067bad425a9da0bd5af9279
Howard Butler <howard@hobu.co>
2020-09-08 11:17:05 -0500

    major reorg

--- python-wheel-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /src/python
-/opt/python/cp37-cp37m/bin/python setup.py sdist
-/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
-auditwheel -v repair ./dist/*linux_x86_64.whl
-rm ./dist/*linux_x86_64.whl
-cp wheelhouse/*.whl dist
