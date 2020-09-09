fcf014dc850ab6abd399abe2d69a4150ca98a2a0
Howard Butler <howard@hobu.co>
2020-09-09 09:10:10 -0500

    wheel test

--- cpp/emscripten-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$EMSCRIPTEN" ] ; then
-	echo "You need to set the EMSCRIPTEN environment variable."
-	exit 1
-fi
-
-cmake . -DCMAKE_TOOLCHAIN_FILE="$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake" \
-	-DCMAKE_BUILD_TYPE=Release
-$EMSCRIPTEN/emmake make VERBOSE=1
