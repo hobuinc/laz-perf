0f8dc476977c7ea9e12ea2a6b842910f8f17857d
Andrew Bell <andrew.bell.ia@gmail.com>
2021-03-02 13:01:23 -0500

    Version and base header files.

---
+++ cpp/laz-perf/version.hpp
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+
+  FILE:  version.hpp
+
+  CONTENTS:
+    LAZ vlr
+
+  COPYRIGHT:
+
+    (c) Hobu Inc.
+
+    This is free software; you can redistribute and/or modify it under the
+    terms of the GNU Lesser General Licence as published by the Free Software
+    Foundation. See the COPYING file for more information.
+
+    This software is distributed WITHOUT ANY WARRANTY and without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  CHANGE HISTORY:
+
+===============================================================================
+*/
+
+#pragma once
+
+#define LAZPERF_MAJOR_VERSION 1
+#define LAZPERF_MINOR_VERSION 3
+#define LAZPERF_REVISION 0
+#define LAZPERF_VERSION 1.3.0
