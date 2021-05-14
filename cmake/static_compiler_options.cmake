51cd979058b982d76faa348dcf12bc2dd033ca63
Andrew Bell <andrew.bell.ia@gmail.com>
2021-05-14 09:59:31 -0400

    Update2

--- cmake/static_compiler_options.cmake
+++ cmake/static_compiler_options.cmake
@@ -6,3 +6,12 @@ function(static_target_compile_settings target)
         target_compile_options(${target} PUBLIC -s DISABLE_EXCEPTION_CATCHING=0)
     endif()
 endfunction()
+
+function(static_target_link_settings target)
+    #
+    # We enable exception catching for test/local example programs..
+    #
+    if (EMSCRIPTEN)
+        target_link_options(${target} PUBLIC -s DISABLE_EXCEPTION_CATCHING=0)
+    endif()
+endfunction()
