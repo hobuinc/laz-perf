ac1725e37fb31e1ec7b9e7193cd4bbc0f521ede1
Christopher Lee <csl170000@utdallas.edu>
2021-11-11 08:29:13 -0700

    Force emscripten generator; default build type (#106)
    * force emscripten generator to use makefiles
    
    * default build type for release

--- emscripten-build.sh
+++ emscripten-build.sh
@@ -5,6 +5,7 @@ if [ -z "$EMSDK" ] ; then
 fi
 
 cmake .. \
+	-G "Unix Makefiles"\
     -DCMAKE_TOOLCHAIN_FILE="$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" \
 	-DCMAKE_BUILD_TYPE=Release -DWASM=1
 $EMSDK/upstream/emscripten/emmake make VERBOSE=1
