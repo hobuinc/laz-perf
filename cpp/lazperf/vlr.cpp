8c6aa1b50ab99843263995e7e6fe66b4065d5368
Andrew Bell <andrew.bell.ia@gmail.com>
2022-03-25 13:41:33 -0400

    More deprecated declarations.

--- cpp/lazperf/vlr.cpp
+++ cpp/lazperf/vlr.cpp
@@ -325,8 +325,13 @@ eb_vlr::eb_vlr()
 
 eb_vlr::eb_vlr(int ebCount)
 {
-    while (ebCount--)
-        addField();
+    for (int i = 0; i < ebCount; ++i)
+    {
+        eb_vlr::ebfield field;
+
+        field.name = "FIELD_" + std::to_string(i);
+        ebVlr.addField(field);
+    }
 }
 
 eb_vlr::~eb_vlr()
