f64d36734e71f897c58e6a4e36528a31a5ef686a
Connor Manning <connor@hobu.co>
2022-07-12 14:17:44 -0500

    Autoformat.

--- js/src/test/file.test.ts
+++ js/src/test/file.test.ts
@@ -1,13 +1,13 @@
-import { promises as fs } from "fs";
-import { join } from "path";
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-import { createLazPerf } from "..";
-import { parseHeader } from "./utils";
+import { createLazPerf } from '..'
+import { parseHeader } from './utils'
 
-const testdatadir = join(__dirname, "../../../cpp/test/raw-sets");
+const testdatadir = join(__dirname, '../../../cpp/test/raw-sets')
 
-test("file reader", async () => {
-  const file = await fs.readFile(join(testdatadir, "autzen_trim.laz"));
+test('file reader', async () => {
+  const file = await fs.readFile(join(testdatadir, 'autzen_trim.laz'))
 
   const {
     pointDataRecordFormat,
@@ -17,36 +17,36 @@ test("file reader", async () => {
     offset,
     min,
     max,
-  } = parseHeader(file);
+  } = parseHeader(file)
 
-	// Check some header values for sanity, for example we want to make sure we
-	// don't get a pointCount of 0 and then not perform any data checks.
-  expect(pointDataRecordFormat).toEqual(3);
-  expect(pointDataRecordLength).toEqual(34);
-  expect(pointCount).toEqual(110_000);
-  expect(scale).toEqual([0.01, 0.01, 0.01]);
-  expect(min.every((v, i) => v < max[i])).toBe(true);
+  // Check some header values for sanity, for example we want to make sure we
+  // don't get a pointCount of 0 and then not perform any data checks.
+  expect(pointDataRecordFormat).toEqual(3)
+  expect(pointDataRecordLength).toEqual(34)
+  expect(pointCount).toEqual(110_000)
+  expect(scale).toEqual([0.01, 0.01, 0.01])
+  expect(min.every((v, i) => v < max[i])).toBe(true)
 
   // Create our Emscripten module.
-  const LazPerf = await createLazPerf();
-  const laszip = new LazPerf.LASZip();
+  const LazPerf = await createLazPerf()
+  const laszip = new LazPerf.LASZip()
 
   // Allocate our memory in the Emscripten heap: a filePtr buffer for our
   // compressed content and a single point's worth of bytes for our output.
-  const dataPtr = LazPerf._malloc(pointDataRecordLength);
-  const filePtr = LazPerf._malloc(file.byteLength);
+  const dataPtr = LazPerf._malloc(pointDataRecordLength)
+  const filePtr = LazPerf._malloc(file.byteLength)
 
   // Copy our data into the Emscripten heap so we can point at it in getPoint().
   LazPerf.HEAPU8.set(
     new Uint8Array(file.buffer, file.byteOffset, file.byteLength),
     filePtr
-  );
+  )
 
   try {
-    laszip.open(filePtr, file.byteLength);
+    laszip.open(filePtr, file.byteLength)
 
     for (let i = 0; i < pointCount; ++i) {
-      laszip.getPoint(dataPtr);
+      laszip.getPoint(dataPtr)
 
       // Now our dataPtr (in the Emscripten heap) contains our point data, copy
       // it out into our own Buffer.
@@ -54,7 +54,7 @@ test("file reader", async () => {
         LazPerf.HEAPU8.buffer,
         dataPtr,
         pointDataRecordLength
-      );
+      )
 
       // Grab the scaled/offset XYZ values and reverse the scale/offset to get
       // their absolute positions.  It would be possible to add checks for
@@ -64,18 +64,18 @@ test("file reader", async () => {
         pointbuffer.readInt32LE(0),
         pointbuffer.readInt32LE(4),
         pointbuffer.readInt32LE(8),
-      ].map((v, i) => v * scale[i] + offset[i]);
+      ].map((v, i) => v * scale[i] + offset[i])
 
       // Doing 6 expect(point[n]).toBeGreaterThanOrEqual(min[n]) style checks in
       // this tight loop slows down the test by 50x, so do a quicker check.
       if (point.some((v, i) => v < min[i] || v > max[i])) {
-        console.error(i, point, min, max);
-        throw new Error(`Point ${i} out of expected range`);
+        console.error(i, point, min, max)
+        throw new Error(`Point ${i} out of expected range`)
       }
     }
   } finally {
-    LazPerf._free(filePtr);
-    LazPerf._free(dataPtr);
-    laszip.delete();
+    LazPerf._free(filePtr)
+    LazPerf._free(dataPtr)
+    laszip.delete()
   }
-});
+})
--- js/src/test/utils.ts
+++ js/src/test/utils.ts
@@ -3,41 +3,41 @@
  * data contents.
  */
 export function parseHeader(file: Buffer) {
-  if (file.byteLength < 227) throw new Error("Invalid file length");
+  if (file.byteLength < 227) throw new Error('Invalid file length')
 
-  const pointDataRecordFormat = file.readUint8(104) & 0b1111;
-  const pointDataRecordLength = file.readUint16LE(105);
-  const pointDataOffset = file.readUint32LE(96);
-  const pointCount = file.readUint32LE(107);
+  const pointDataRecordFormat = file.readUint8(104) & 0b1111
+  const pointDataRecordLength = file.readUint16LE(105)
+  const pointDataOffset = file.readUint32LE(96)
+  const pointCount = file.readUint32LE(107)
 
   const scale = [
     file.readDoubleLE(131),
     file.readDoubleLE(139),
     file.readDoubleLE(147),
-  ];
+  ]
   const offset = [
     file.readDoubleLE(155),
     file.readDoubleLE(163),
     file.readDoubleLE(171),
-  ];
+  ]
   const min = [
     file.readDoubleLE(187),
     file.readDoubleLE(203),
     file.readDoubleLE(219),
-  ];
+  ]
   const max = [
     file.readDoubleLE(179),
     file.readDoubleLE(195),
     file.readDoubleLE(211),
-  ];
+  ]
   return {
     pointDataRecordFormat,
     pointDataRecordLength,
-		pointDataOffset,
+    pointDataOffset,
     pointCount,
     scale,
     offset,
     min,
     max,
-  };
+  }
 }
