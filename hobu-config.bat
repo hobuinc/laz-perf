ae194fc7e647e5abf119e53b9ef8cb86c1a6da95
Howard Butler <howard@hobu.co>
2023-01-11 15:07:17 -0600

    Adapt warnings fixes from Andrew's branch

--- hobu-config.bat
+++ hobu-config.bat
@@ -1,25 +1,18 @@
 @echo on
 
-:: This configure script is designed for the default Windows world, 
-
-:: This configure script expects to be run from the PDAL root directory.
-
-:: Pick your CMake GENERATOR.  (NMake will pick up architecture (x32, x64) from your environment)
-set GENERATOR="NMake Makefiles"
-REM set GENERATOR="Visual Studio 14 Win64"
-rem set GENERATOR="Visual Studio 10"
-
-:: Pick your build type
+set GENERATOR="Ninja"
 set BUILD_TYPE=Release
-REM set BUILD_TYPE=Debug
 
+del /s /q %CONDA_DEFAULT_ENV%
+mkdir %CONDA_DEFAULT_ENV%
+cd %CONDA_DEFAULT_ENV%
 
-if EXIST CMakeCache.txt del CMakeCache.txt
 
 cmake -G %GENERATOR% ^
-    -DCMAKE_INSTALL_PREFIX=c:/OSGeo4W64 ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+     -DCMAKE_BUILD_TYPE=Release -Dgtest_force_shared_crt=ON ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
-	-DBUILD_SHARED_LIBS=ON ^
-    .
-	
+    ..
+    
+
+ninja
