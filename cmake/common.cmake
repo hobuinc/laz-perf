284f2afcb6ce0201cd30e7e675e95e81de0b3d10
Howard Butler <hobu.inc@gmail.com>
2026-01-01 11:08:09 -0600

    need install.cmake ahead of functions.cmake

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,5 +1,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/directories.cmake)
 
+include(${CMAKE_CURRENT_LIST_DIR}/install.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/functions.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/compiler_options.cmake)
 
